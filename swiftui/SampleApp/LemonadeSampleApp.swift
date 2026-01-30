5944ac47bae2375fd31bfb9a41278e4dc5e2fae7
Felipe Marcon <felipeemarcon@gmail.com>
2026-01-30 17:56:01 -0300

    feat: force Figtree as the default ui font

--- swiftui/SampleApp/LemonadeSampleApp.swift
+++ swiftui/SampleApp/LemonadeSampleApp.swift
@@ -6,11 +6,25 @@ struct LemonadeSampleApp: App {
     init() {
         // Register Lemonade fonts at app startup
         LemonadeFonts.registerFonts()
+        
+        // Customize the font for large titles
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .font: UIFont(name: "Figtree-Semibold" ,size: LemonadeTypography.shared.headingLarge.fontSize)!
+        ]
+        
+        // Customize the font for inline titles
+        UINavigationBar.appearance().titleTextAttributes = [
+            .font: UIFont(name: "Figtree-Semibold", size: LemonadeTypography.shared.headingXXSmall.fontSize)!
+        ]
+        
+        // Customize the fon for search bar
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).font = UIFont(name: "Figtree", size: LemonadeTypography.shared.bodyMediumMedium.fontSize)
     }
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.font, Font.custom("Figtree", size: LemonadeTypography.shared.bodyMediumMedium.fontSize))
         }
     }
 }
