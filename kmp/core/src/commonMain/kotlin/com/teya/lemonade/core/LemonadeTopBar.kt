55b0d2f0b7b92cb4d271ba0911ce6ad2ecb84133
Willian Mota Oliveira <willian.oliveira@saltpay.co>
2026-03-18 15:25:10 +0000

    fix unnecessary changes

--- kmp/core/src/commonMain/kotlin/com/teya/lemonade/core/LemonadeTopBar.kt
+++ kmp/core/src/commonMain/kotlin/com/teya/lemonade/core/LemonadeTopBar.kt
@@ -4,11 +4,5 @@ package com.teya.lemonade.core
 
 public enum class TopBarAction {
     Back,
-    BackBoxed,
     Close,
-    CloseBoxed,
-    ;
-
-    public val hasBackground: Boolean
-        get() = this == BackBoxed || this == CloseBoxed
 }
