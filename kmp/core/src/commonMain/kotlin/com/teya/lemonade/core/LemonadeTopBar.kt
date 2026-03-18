852fb72fee0e975f143a6cb23b67fe4b22e334a9
Filipe Alves <filipe.ramos@saltpay.co>
2026-03-18 11:51:54 +0000

    feat: added support for topbars to be compatible with bottomsheets and big collapsable title

--- kmp/core/src/commonMain/kotlin/com/teya/lemonade/core/LemonadeTopBar.kt
+++ kmp/core/src/commonMain/kotlin/com/teya/lemonade/core/LemonadeTopBar.kt
@@ -4,5 +4,11 @@ package com.teya.lemonade.core
 
 public enum class TopBarAction {
     Back,
+    BackBoxed,
     Close,
+    CloseBoxed,
+    ;
+
+    public val hasBackground: Boolean
+        get() = this == BackBoxed || this == CloseBoxed
 }
