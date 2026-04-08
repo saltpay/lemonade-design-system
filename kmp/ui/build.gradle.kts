c3435a5e0301ac86bf639c6c4fc47c6b7be57402
Willian Mota Oliveira <willian.oliveira@saltpay.co>
2026-04-08 17:47:15 +0100

    revert breaking test

--- kmp/ui/build.gradle.kts
+++ kmp/ui/build.gradle.kts
@@ -29,7 +29,6 @@ kotlin {
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.detekt.gradlePlugin)
             api(projects.tokens)
             api(projects.core)
         }
