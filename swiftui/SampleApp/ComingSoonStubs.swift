658b1a565f50152644f0ef7e5903162b1b6d3b52
Raouf Rahiche <raouf.rahiche@saltpay.co>
2026-04-30 22:00:00 +0100

    chore(sample): add ComingSoonStubs for unreleased components; use xcodegen for CLI builds

---
+++ swiftui/SampleApp/ComingSoonStubs.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+// Stubs for components not yet implemented in this version of Lemonade
+struct MarkdownDisplayView: View {
+    var body: some View { Text("Coming soon").foregroundColor(.gray).padding() }
+}
+
+struct InlineCalendarDisplayView: View {
+    var body: some View { Text("Coming soon").foregroundColor(.gray).padding() }
+}
+
+struct HistoryTimelineDisplayView: View {
+    var body: some View { Text("Coming soon").foregroundColor(.gray).padding() }
+}
